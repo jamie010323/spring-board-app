6c584587c3dbb1567a00e1549c30b2fec84551ec
jamie010323 <jamie010323@gmail.com>
2026-06-29 10:48:09 +0900

    add board

---
+++ src/main/java/kr/co/sboard/dto/CommentDTO.java
@@ -0,0 +1,33 @@
+package kr.co.sboard.dto;
+
+import kr.co.sboard.entity.Comment;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CommentDTO {
+
+    private int cno;
+    private int parent;
+    private String content;
+    private String writer;
+    private String regip;
+    private String wdate;
+
+    public Comment toEntity(){
+        return Comment.builder()
+                    .cno(cno)
+                    .parent(parent)
+                    .content(content)
+                    .writer(writer)
+                    .regip(regip)
+                    .build();
+    }
+
+}
